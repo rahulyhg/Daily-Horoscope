d2b2e37b735569a25749022d093af32def168e03
garganjohn <garganjohn@gmail.com>
2019-03-02 16:48:20 -0500

    displayhoroscope now has relevant horoscope info displayed

--- app/src/main/java/org/pursuit/dailyhoroscope/network/HoroscopeService.java
+++ app/src/main/java/org/pursuit/dailyhoroscope/network/HoroscopeService.java
@@ -6,6 +6,7 @@ import org.pursuit.dailyhoroscope.model.Signs;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface HoroscopeService {
@@ -13,7 +14,7 @@ public interface HoroscopeService {
     Call<HoroApi> getApi();
 
     @GET("theastrologer/api/horoscope/{sunsign}/today")
-    Call<Horoscope> getHoroscope(@Query("sunsign") String sign);
+    Call<Horoscope> getHoroscope(@Path("sunsign") String sign);
 
     @GET ("theastrologer/api/sunsigns/")
     Call<String[]> getSignsEndPoint();
